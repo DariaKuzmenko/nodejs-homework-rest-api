bb6c707371165f2bd000d5931034578520b01f83
DariaKuzmenko <103844653+DariaKuzmenko@users.noreply.github.com>
2023-11-05 02:19:27 +0100

    hw05 is done

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,9 +1,16 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import fs from "fs/promises";
+import path from "path";
+import gravatar from "gravatar";
+import jimp from "jimp";
 
+import User from "../models/User.js";
 import { HttpError } from "../helpers/index.js";
 import { ctrlWrapper } from "../decorators/index.js";
+import Jimp from "jimp";
+
+const avatarPath = path.resolve("public", "avatars");
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
@@ -14,13 +21,21 @@ const signup = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
 
+  const avatarURL = gravatar.url(email, {
+    s: "200",
+    r: "pg",
+    d: "404",
+  });
+
   const newUser = await User.create({
     ...req.body,
+    avatarURL,
     password: hashPassword,
   });
 
   res.status(201).json({
     email: newUser.email,
+    avatarURL: newUser.avatarURL,
     subscription: newUser.subscription,
   });
 };
@@ -45,6 +60,10 @@ const signin = async (req, res) => {
 
   res.json({
     token,
+    user: {
+      email: user.email,
+      subscription: user.subscription,
+    },
   });
 };
 
@@ -60,9 +79,44 @@ const signout = async (req, res) => {
   res.json({ message: "Signout success" });
 };
 
+const updateSubscription = async (req, res) => {
+  const { id } = req.params;
+  const { subscription } = req.body;
+
+  if (!subscription) {
+    throw HttpError(401);
+  }
+  const result = await User.findByIdAndUpdate(id, { subscription });
+
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json({ subscription: result.subscription });
+};
+
+const updateAvatar = async (req, res) => {
+  const { path: oldPath, filename } = req.file;
+  const newPath = path.join(avatarPath, filename);
+
+  const image = await Jimp.read(oldPath);
+  image.resize(250, 250).write(newPath);
+
+  await fs.unlink(oldPath);
+
+  const avatarURL = path.join("avatars", filename);
+
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.status(200).json({
+    avatarURL,
+  });
+};
+
 export default {
   signup: ctrlWrapper(signup),
   signin: ctrlWrapper(signin),
   getCurrent: ctrlWrapper(getCurrent),
   signout: ctrlWrapper(signout),
+  updateSubscription: ctrlWrapper(updateSubscription),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
